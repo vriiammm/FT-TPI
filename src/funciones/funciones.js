2c1b0524763670228ca844ef19cd8da6b9878e47
Brian <brian040408.z@gmail.com>
2026-09-09 19:36:58 -0300

    se agregaron 2 nuevas funciones

--- src/funciones/funciones.js
+++ src/funciones/funciones.js
@@ -1,3 +1,11 @@
 export function suma(a, b) {
     return a + b;
 }
+
+export function esMayorDeEdad(edad){
+    return edad >= 18;
+}
+
+export function saludar(nombre) {
+    return `Hola, ${nombre}`;
+}
